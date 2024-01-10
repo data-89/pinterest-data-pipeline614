8cd080595790941f177d122dee9a3eca61094e37
rafal <rafal@MacBook-Air-rafal-2.local>
2024-01-10 20:08:32 +0000

    Emulator update with payloads

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -42,32 +42,55 @@ def run_infinite_post_data_loop():
             
             for row in pin_selected_row:
                 pin_result = dict(row._mapping)
+                payload_pin = json.dumps({
+                "records": [
+                    {
+                    "value": {"index": pin_result["index"], "unique_id": pin_result["unique_id"], "title": pin_result["title"], "description": pin_result["description"], "poster_name": pin_result["poster_name"], "follower_count": pin_result["follower_count"], "tag_list": pin_result["tag_list"], "is_image_or_video": pin_result["is_image_or_video"], "image_src": pin_result["image_src"], "downloaded": pin_result["downloaded"], "save_location": pin_result["save_location"], "category": pin_result["category"]}
+                    }
+                    ]
+                })
 
             geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
             geo_selected_row = connection.execute(geo_string)
             
             for row in geo_selected_row:
                 geo_result = dict(row._mapping)
+                payload_geo = json.dumps({
+                "records": [
+                    {
+                    "value": {"ind": geo_result["ind"], "timestamp": geo_result["timestamp"], "latitude": geo_result["latitude"], "longitude": geo_result["longitude"], "country": geo_result["country"]}
+                    }
+                    ]
+                })
 
             user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
             user_selected_row = connection.execute(user_string)
             
             for row in user_selected_row:
                 user_result = dict(row._mapping)
+                payload_user = json.dumps({
+                "records": [
+                    {
+                    #Data should be send as pairs of column_name:value, with different columns separated by commas       
+                    "value": {"ind": user_result["ind"], "first_name": user_result["first_name"], "last_name": user_result["last_name"], "age": user_result["age"], "date_joined": user_result["date_joined"]}
+                    }
+                    ]
+                })
             
             print(pin_result)
             print(geo_result)
             print(user_result)
+            
 
             invoke_url_pin = "https://7txpgfi8ok.execute-api.us-east-1.amazonaws.com/dev/topics/12951463f185.pin"
             invoke_url_geo = "https://7txpgfi8ok.execute-api.us-east-1.amazonaws.com/dev/topics/12951463f185.geo"
             invoke_url_user = "https://7txpgfi8ok.execute-api.us-east-1.amazonaws.com/dev/topics/12951463f185.user"
 
             headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
-            response_pin = requests.request("POST", invoke_url_pin, headers=headers, data=pin_result)
+            response_pin = requests.request("POST", invoke_url_pin, headers=headers, data=payload_pin)
             print(response_pin.status_code)
-            response_geo = requests.request("POST", invoke_url_geo, headers=headers, data=geo_result)
-            response_user = requests.request("POST", invoke_url_user, headers=headers, data=user_result)
+            response_geo = requests.request("POST", invoke_url_geo, headers=headers, data=payload_geo)
+            response_user = requests.request("POST", invoke_url_user, headers=headers, data=payload_user)
 
 
 if __name__ == "__main__":
